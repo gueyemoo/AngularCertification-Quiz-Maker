33b06beb17244f5bea8c676993b054a064087c34
gueyemo <m.gueye0411@gmail.com>
2023-07-16 20:42:07 +0200

    add new function to retrieve quiz questions in service

--- src/app/common/services/trivia.service.ts
+++ src/app/common/services/trivia.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { CategoriesResponse } from '../models/trivia-input.model';
+import { QuizQuestion } from '../models/trivia-quiz.model';
+import { AMOUNT_OF_QUESTIONS, QUESTION_TYPE } from '../helpers/utils';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,18 @@ export class TriviaService {
   getCategories(): Observable<CategoriesResponse> {
     const categoriesUrl = `${this.apiUrl}/api_category.php`;
     return this.http.get<CategoriesResponse>(categoriesUrl);
+  }
+
+  getQuizQuestions(
+    category: number,
+    difficulty: string
+  ): Observable<QuizQuestion[]> {
+    const amount = AMOUNT_OF_QUESTIONS;
+    const type = QUESTION_TYPE;
+
+    const req_url = `${this.apiUrl}/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
+    return this.http
+      .get<{ results: QuizQuestion[] }>(req_url)
+      .pipe(map((res) => res.results));
   }
 }
