378cb2ae6f91431073fca583754cd16cc2a38da7
gueyemo <m.gueye0411@gmail.com>
2023-07-17 22:04:11 +0200

    update routing module for new result component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TriviaResultComponent } from './main-page/trivia-result/trivia-result.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
+  },
+  {
+    path: 'results',
+    component: TriviaResultComponent,
   },
 ];
 
